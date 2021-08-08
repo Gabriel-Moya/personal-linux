685514f6d936589f6b8fcbdc6ed3229b8f89ab53
Gabriel Moya <gabrielmoya123@gmail.com>
2021-08-07 21:56:38 -0400

    Initial commit

---
+++ virtualizations/start-vms-nogui.sh
@@ -0,0 +1,24 @@
+# Start virtual machines
+
+##
+# File Server
+##
+/usr/bin/vmrun start "/home/gabriel/vmware/file-server/file-server.vmx" nogui
+
+
+##
+# WP Server
+##
+/usr/bin/vmrun start "/home/gabriel/vmware/wp-server/wp-server.vmx" nogui
+
+
+##
+# Code Server
+##
+/usr/bin/vmrun start "/home/gabriel/vmware/code-server/code-server.vmx" nogui
+
+
+##
+# BD server moderniza
+##
+/usr/bin/vmrun start "/home/gabriel/vmware/bd-server-moderniza/bd-server-moderniza.vmx" nogui
---
+++ virtualizations/stop-vms-nogui.sh
@@ -0,0 +1,24 @@
+# Stop virtual machines
+
+##
+# File Server
+##
+/usr/bin/vmrun stop "/home/gabriel/vmware/file-server/file-server.vmx" nogui
+
+
+##
+# WP Server
+##
+/usr/bin/vmrun stop "/home/gabriel/vmware/wp-server/wp-server.vmx" nogui
+
+
+##
+# Code Server
+##
+/usr/bin/vmrun stop "/home/gabriel/vmware/code-server/code-server.vmx" nogui
+
+
+##
+# BD server moderniza
+##
+/usr/bin/vmrun stop "/home/gabriel/vmware/bd-server-moderniza/bd-server-moderniza.vmx" nogui
